79168858c1e8514ef7d890d0bda43609acd775db
olav <olav@bjorkoy.com>
2012-01-13 16:28:31 +0100

    Object.create for nettlesere som ikke implementerer dette selv.

---
+++ js/extension/object.js
@@ -0,0 +1,11 @@
+// Object.create() for nettlesere som ikke implementerer denne selv.
+if (!Object.create) {
+    Object.create = function (o) {
+        if (arguments.length > 1) {
+            throw new Error('Object.create implementation only accepts the first parameter.');
+        }
+        function F() {}
+        F.prototype = o;
+        return new F();
+    };
+}
