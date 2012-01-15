5a8c4fca60f9e949a1341f0205103838b776ac34
olav <olav@bjorkoy.com>
2012-01-15 13:17:30 +0100

    Vise at viewmodeller kan ha flere funksjoner enn compile og apply.

--- js/view/shared/searchbox.js
+++ js/view/shared/searchbox.js
@@ -8,19 +8,17 @@ myapp.view.shared.searchbox = myapp.model.create(myapp.model.viewmodel, {
 
     apply: function() {
         var that = this;
-
         $("#search-input").on("keyup", function (event) {
             that.search(); 
         });
+    },
 
-        that.search = function() {
-            var searchString = $('#search-input').val();
+    search: function() {
+        var searchString = $('#search-input').val();
 
-            if (searchString === "") {
-                $("li").show();
-                return;
-            }
-
+        if (searchString === "") {
+            $("li").show();
+        } else {
             var list = $("ul");
             list.find("li:not(:contains(" + searchString + "))").hide();
             list.find("li:contains(" + searchString + ")").show();    
